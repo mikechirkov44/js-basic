15be648094293a9e7a8d7ca5c8ebbc1bde4ab4a6
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-03-27 12:13:18 +0300

    Lesson_2

---
+++ l2_5.js
@@ -0,0 +1,21 @@
+//Реализовать четыре основные арифметические операции в виде функций с двумя
+//параметрами. Обязательно использовать оператор return.
+
+let a = 2, b = 3;
+
+function plus(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function mult(a, b) {
+    return a + b;
+}
+
---
+++ l2_6.js
@@ -0,0 +1,24 @@
+/* Задача 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, 	
+arg2 – значения аргументов, operation – строка с названием операции. В зависимости от переданного 		
+значения операции выполнить одну из арифметических операций (использовать функции из пункта 3) и 		
+вернуть полученное значение (использовать switch). */
+
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case 'sum':
+            return arg1 + arg2;
+            break;
+        case 'minus':
+            return arg1 - arg2;
+            break;
+        case 'mult':
+            return arg1 * arg2;
+            break;
+        case 'div':
+            return arg1 / arg2;
+            break;
+    }
+
+}
+
+console.log(mathOperation(2, 4, 'minus'))
